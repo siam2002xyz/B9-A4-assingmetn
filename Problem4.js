0bb249cba57c9c8dfdf1d3c19a5d6a014651dd0e
siam2002xyz <siamhasan2002x@gmail.com>
2024-02-13 16:51:20 +0600

    Problem4 solved

--- Problem4.js
+++ Problem4.js
@@ -24,5 +24,6 @@ function monthlySavings(arr, livingCost) {
 }
 
 console.log(monthlySavings([ 1000 , 2000 , 2500 ] , 5000));
+console.log(monthlySavings([ 10000 , 20000 , 25000 ] , 50000));
 console.log(monthlySavings([ 900 , 2700 , 3400] , 10000));
 
